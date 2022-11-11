b6b459a0de8837863239435b7c2a3c23af91d600
tulip-sudo <noctaliavibing@gmail.com>
2022-11-11 20:40:35 +0000

    Update script

---
+++ update.sh
@@ -0,0 +1,4 @@
+# CURL TIME WOOOOOOOOOOOOO0000OOOO)o0o0o0o0o0
+curl https://raw.githubusercontent.com/Sprinter05/ytdlp-handler/main/start.sh -o start.sh
+curl https://raw.githubusercontent.com/Sprinter05/ytdlp-handler/main/settings.ini -o nsettings.ini
+echo "New version of start.sh and settings.ini have been downloaded.\nTo apply the new settings, replace settings.ini with nsettings.ini"
