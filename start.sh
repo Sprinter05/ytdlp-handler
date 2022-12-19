c9759641fa1b10f7937f61670433c01305d81f82
Sprinter05 <sprinterhr@gmail.com>
2022-12-19 18:05:39 +0100

    oopsie

--- start.sh
+++ start.sh
@@ -105,14 +105,14 @@ if [ $LEGACY == 1 ]: then
 	echo -n "Checking dependencies... "
 	for name in youtube-dl ffmpeg
 	do
-  	[[ $(which $name 2>/dev/null) ]] || { echo -en "\n$name needs to be installed. Use 'sudo apt-get install $name'";deps=1; }
+  	[[ $(which $name 2>/dev/null) ]] || { echo -en "\n$name needs to be installed.";deps=1; }
 	done
 	[[ $deps -ne 1 ]] && echo "OK" || { echo -en "\nInstall the above and rerun this script\n";exit 1; }
 else
 	echo -n "Checking dependencies... "
 	for name in yt-dlp ffmpeg
 	do
-  	[[ $(which $name 2>/dev/null) ]] || { echo -en "\n$name needs to be installed. Use 'sudo apt-get install $name'";deps=1; }
+  	[[ $(which $name 2>/dev/null) ]] || { echo -en "\n$name needs to be installed.";deps=1; }
 	done
 	[[ $deps -ne 1 ]] && echo "OK" || { echo -en "\nInstall the above and rerun this script\n";exit 1; }
 fi
