6e867a2b6594dc9aa79f6dbf04ae166ffe3d75d6
tulip-sudo <noctaliavibing@gmail.com>
2022-12-19 17:00:12 +0000

    Funky redo of update.sh

--- update.sh
+++ update.sh
@@ -1,15 +1,41 @@
-# CURL TIME WOOOOOOOOOOOOO0000OOOO)o0o0o0o0o0
-CSHA=$(sha512sum start.sh)
-ONLINESHA=$(curl -L https://github.com/Sprinter05/ytdlp-handler/releases/latest/download/start.sh.sha512)
-
-if [[ "$CSHA" != "$ONLINESHA" ]]; then
-	curl -L https://github.com/Sprinter05/ytdlp-handler/releases/latest/download/start.sh -o start.sh
-	if [ -f settings.ini ]; then
-		curl -L https://github.com/Sprinter05/ytdlp-handler/releases/latest/download/settings.ini -o nsettings.ini
-		echo "New version of start.sh and settings.ini have been downloaded.\nTo apply the new settings, replace settings.ini with nsettings.ini"
-	else
-		curl -L https://github.com/Sprinter05/ytdlp-handler/releases/latest/download/settings.ini -o settings.ini # Where'd your settings go
-	fi
-else
-	echo "You already have the latest version."
+if [ -f update_new.sh ]; then
+	echo Rename this file to update.sh!
+	read -p "Press enter to continue... "
+	exit
 fi
+
+echo "Would you like to reset your current settings to their default values? [y/n]"
+read -p "> " DFSET
+if [ "$DFSET" == "y" ]; then
+	RESETSET=1
+else
+	RESETSET=0
+fi
+echo ""
+
+curl -L https://github.com/Sprinter05/ytdlp-handler/releases/latest/download/ytdlp-handler_linux.zip -o ytdlp-handler_linux.zip
+mkdir temp
+unzip ytdlp-handler_linux.zip ./temp/
+if [ -f "start.sh" ]; then
+	rm "./start.sh"
+fi
+if [ -f "changelog.txt" ]; then
+	rm "./changelog.txt"
+fi
+mv "./temp/update.sh" "update_new.sh"
+mv "./temp/update_new.sh" "./"
+mv "./temp/start.sh" "./"
+mv "./temp/start_win.sh" "./"
+mv "./temp/changelog.txt" "./"
+if [ $RESETSET == 1 ]; then
+	if [ -f settings.ini ]; then
+		rm "./settings.ini"
+	fi
+	mv "./temp/settings.ini" "./"
+fi
+rm -r "./temp"
+rm "./ytdlp-handler_linux.zip"
+echo
+read -p "Rename your update_new.sh file to update.sh! "
+rm "./update.sh"
+# Code by Sprinter05
